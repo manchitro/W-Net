32faad57e2b6d9ca030c4d635f1606d766c482d3
Sazid <initialsaremsu@gmail.com>
2023-11-04 15:38:53 +0600

    bug

--- Wnet/DataLoader.py
+++ Wnet/DataLoader.py
@@ -105,7 +105,7 @@ class DataLoader():
 
     def get_dataset(self,raw_data,shape,batch_size):
         dataset = []
-        print("shape: " + shape)
+        print("shape: " + str(shape))
         for batch_id in range(0,shape[0],batch_size):
             print("batch_id: " + batch_id)
             batch = raw_data[batch_id:min(shape[0],batch_id+batch_size)]
